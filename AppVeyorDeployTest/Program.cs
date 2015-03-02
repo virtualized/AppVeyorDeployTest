5142e366a9ce227c0b7c4345eeaeef85da42b6d3
James Bartram <james@v5.me.uk>
2015-03-02 21:37:44 +0000

    Added console output

---
+++ AppVeyorDeployTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppVeyorDeployTest
+{
+    class Program
+    {
+        [STAThread]
+        public static int Main()
+        {
+            var obj = new TestClass();
+            Console.WriteLine(obj.GetSomeString());
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
